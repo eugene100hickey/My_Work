64e54c2c101a88dba3008b158dc1276f4aee3fc8
eugene100hickey <eugene.hickey@tudublin.ie>
2022-04-03 15:11:05 +0100

    4 x models

--- primel.R
+++ primel.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(matlab)
 
-begin <- 50631
-end <- 100000
-my_step <- 10000
+begin <- 30023
+end <- 99999
+my_step <- 1000
 
 numbers <- seq(begin, end, by = my_step)
 
 numbers[isprime(numbers)==1]
 
-numbers[isprime(numbers)==1 & between(numbers %% 1000, 800, 900)]
+numbers[isprime(numbers)==1 & between(numbers %% 10000, 2000, 2100)]
